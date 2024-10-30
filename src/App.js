ad780a29574b168849adc52fbf0719e322c69895
kiran <udaykiransura143@gmail.com>
2024-10-30 20:06:49 +0530

    just for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/inner';
+import TextPlace from './components/TextPlace';
+// let a = <img src="public/logo512.png" alt="" />
+// let a ='pink'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar name = "Textutils" about="About Us"/>
+    <div className="container ">
+      <TextPlace head="Enter the text to be analysed"/>
     </div>
+      
+
+
+     
+  
+    </>
+   
   );
 }
 
